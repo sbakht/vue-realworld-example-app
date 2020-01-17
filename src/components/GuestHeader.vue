b0268f69a66d5be85efd7ed9916582e57b84a4fb
Saad Bakht <saad.bakht@bloomingdales.com>
2020-01-17 15:09:49 -0500

    break out header

---
+++ src/components/GuestHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="nav navbar-nav pull-xs-right">
+    <li class="nav-item">
+      <router-link
+        class="nav-link"
+        active-class="active"
+        exact
+        :to="{ name: 'home' }"
+      >
+        Home
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link
+        class="nav-link"
+        active-class="active"
+        exact
+        :to="{ name: 'login' }"
+      >
+        <i class="ion-compose"></i>Sign in
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link
+        class="nav-link"
+        active-class="active"
+        exact
+        :to="{ name: 'register' }"
+      >
+        <i class="ion-compose"></i>Sign up
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "GuestHeader",
+  };
+</script>
